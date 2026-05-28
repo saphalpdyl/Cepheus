75cafdd50029aa43e1dced663adf708f5b34d5b2
saphalpdyl <saphalpdyl@gmail.com>
2026-05-28 03:54:03 -0500

    refactor: add new configuration with new executor

--- db/init/04-seed.sql
+++ db/init/04-seed.sql
@@ -8,7 +8,7 @@ VALUES ('da36794e-83a3-4e2c-b799-b7b4e7b68768', 'stamp-to-sa', 'stamp-sender', t
 ON CONFLICT DO NOTHING;
 
 INSERT INTO agent_task (agent_config_id, task_id, type, enabled, generation, schedule_interval_seconds, schedule_jitter_percent, schedule_enabled, params)
-VALUES ('da36794e-83a3-4e2c-b799-b7b4e7b68768', 'trace-to-sa', 'trace', true, 1, 40, 10, true, '{"target": "1.1.1.1", "method": "icmp-paris"}')
+VALUES ('da36794e-83a3-4e2c-b799-b7b4e7b68768', 'trace-to-sa', 'trace', true, 1, 40, 10, true, '{"target": "10.0.0.6", "method": "icmp-paris"}')
 ON CONFLICT DO NOTHING;
 
 -- Reflector config
@@ -18,6 +18,22 @@ ON CONFLICT DO NOTHING;
 
 INSERT INTO agent_task (agent_config_id, task_id, type, enabled, generation, schedule_interval_seconds, schedule_jitter_percent, schedule_enabled, params)
 VALUES ('fff05400-686d-46fe-b6d7-a660569f5228', 'stamp-reflector', 'stamp-reflector', true, 1, 0, 0, false, '{"listen_port": 862, "dscp": 0, "require_clock_sync": false, "source_ip": "10.0.0.6"}')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO agent_task (agent_config_id, task_id, type, enabled, generation, schedule_interval_seconds, schedule_jitter_percent, schedule_enabled, params)
+VALUES ('fff05400-686d-46fe-b6d7-a660569f5228', 'ping-to-cloudflare', 'ping', true, 1, 20, 10, true, '{"target": "1.1.1.1", "count": 5, "size": 64}')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO agent_task (agent_config_id, task_id, type, enabled, generation, schedule_interval_seconds, schedule_jitter_percent, schedule_enabled, params)
+VALUES ('fff05400-686d-46fe-b6d7-a660569f5228', 'trace-to-cloudflare', 'trace', true, 1, 40, 10, true, '{"target": "1.1.1.1", "method": "icmp-paris"}')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO agent_task (agent_config_id, task_id, type, enabled, generation, schedule_interval_seconds, schedule_jitter_percent, schedule_enabled, params)
+VALUES ('fff05400-686d-46fe-b6d7-a660569f5228', 'ping-to-quad9', 'ping', true, 1, 20, 10, true, '{"target": "9.9.9.9", "count": 5, "size": 64}')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO agent_task (agent_config_id, task_id, type, enabled, generation, schedule_interval_seconds, schedule_jitter_percent, schedule_enabled, params)
+VALUES ('fff05400-686d-46fe-b6d7-a660569f5228', 'trace-to-quad9', 'trace', true, 1, 40, 10, true, '{"target": "9.9.9.9", "method": "icmp-paris"}')
 ON CONFLICT DO NOTHING;
 
 -- Link devices to configs
