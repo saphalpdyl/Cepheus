86b4dd2166b55766f1a2615f11be03dd376d0d83
saphalpdyl <git@saphal.me>
2026-07-04 16:58:23 -0500

    doc(argus): doc.go explaining  package's purpose

---
+++ services/argus/doc.go
@@ -0,0 +1,3 @@
+// Package argus defines the detection and notification service of Cepheus. Each Cepheus' instance will contain its own Argus instance,
+// which will be responsible for detecting faults and anomalies throughout the owned network. 
+package argus
